3b573a3fb666385028cda0e80380bed83e6cefc8
Lucas Wiener <lucas.wiener@trustly.com>
2013-03-22 20:16:29 +0100

    Added many2many

--- src/main.go
+++ src/main.go
@@ -7,11 +7,13 @@
 package main
 
 import (
-	"bug1"
-	"bug2"
+	//"bug1"
+	//"bug2"
+	"many2many"
 )
 
 func main() {
-	bug1.Main()
-	bug2.Main()
+	//bug1.Main()
+	//bug2.Main()
+	many2many.Main()
 }
